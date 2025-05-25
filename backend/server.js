854e0beff6f1beb760c199f870a8b8ee2bb0d190
Shitten <121535045+Shitten@users.noreply.github.com>
2025-05-25 13:29:56 +0000

    Add back end log in route and fron end log in form

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,36 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use(express.json());
+const username = process.env['USERNAME_KEY'];
+const password = process.env['PASSWORD_KEY'];
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
+app.use(express.urlencoded({ extended: true }))
+app.post('/login', (req, res) => {
+  const data = req.body;
+  console.log('Received data:', data);
+  
+    if(data.username === username && data.password === password ){
+  console.log('correct');
+      res.json({ success: true });
+      return
+    }else{
+      console.log('wrong')
+      res.json({ success: false });
+    }
+});
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,39 @@
+const userName = document.getElementById("loginUsername");
+const passWord = document.getElementById("loginPassword");
+const logIn    = document.getElementById('login');
+const fieldCheck = () =>{
+if(userName.value || passWord.value ===''){
+  alert("empty")
+  return
+};
+  if(passWord.value.length < 8){
+    alert('password not valid')
+    return
+  };
+ 
+} ;
+
+logIn.addEventListener('click', (e)=>{
+  e.preventDefault();
+  (async () =>{ 
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    const res = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    }); 
+
+    const data = await res.json();
+    console.log(data);
+    if (data.success){
+      document.getElementById('message').textContent = 'Login successful!';
+    }else{
+      document.getElementById('message').textContent = 'Login failed. Please try again.';
+    }
+  })(); 
+})
 
